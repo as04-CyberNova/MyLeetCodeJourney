f46a1896734ae81c829da27ba12ac7d8b5df9f7a
as04-CyberNova <abhyudayasinha4@gmail.com>
2026-08-19 16:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (10.35%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int ans = 0;
         int n = arr.size();
+        vector<int> prefix(n + 1, 0);
         
         for (int i = 0; i < n; i++) {
-            ans += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+            prefix[i + 1] = prefix[i] + arr[i];
         }
         
-        return ans;
+        int totalSum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int len = 1; i + len <= n; len += 2) {
+                totalSum += prefix[i + len] - prefix[i];
+            }
+        }
+        
+        return totalSum;
     }
 };
